733d2f0f29ef48670507567261763eb241a93d67
ivozandhuis <ivo@zandhuis.nl>
2023-12-21 13:43:06 +0100

    Changed extractor in authority

--- authority/transformer.py
+++ authority/transformer.py
@@ -9,9 +9,9 @@ amount = 0 # if 0, than all records are handled
 xsltproc = PySaxonProcessor(license=False).new_xslt30_processor()
 executable = xsltproc.compile_stylesheet(stylesheet_file="auth2sdo.xsl")
 
-ext_path = Path("extracted_test")
+ext_path = Path("extracted")
 for src_file in ext_path.glob("**/*.xml"):
-    out_file = Path('transformed_test').joinpath(*src_file.parts[1:])
+    out_file = Path('transformed').joinpath(*src_file.parts[1:])
     out_file = out_file.with_suffix('.rdf')
     out_file.parent.mkdir(parents=True, exist_ok=True)
     executable.transform_to_file(source_file=str(src_file), output_file=str(out_file))
