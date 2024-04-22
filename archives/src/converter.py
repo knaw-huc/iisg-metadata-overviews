26a50a2a10019865aff5ec8a20d48bdb44bf4c49
lilimelgar <lilimelgar@users.noreply.github.com>
2024-04-22 12:26:52 +0200

    Adjustments to archives transformer
    Replaced original transformer (from EAD to RiC-O) into a newly created transformer.py to convert from MARC to a tabular format, using the example from the transformer in "biblio".

---
+++ archives/src/converter.py
@@ -0,0 +1,51 @@
+
+from pathlib import Path # iterates through all files
+import pandas as pd
+import numpy as np
+import csv
+import re
+#to add timestamp to file names
+import time
+#import os.path to add paths to files
+import os
+
+# create directory and store file
+directory_converted = 'converted'
+os.mkdir(directory_converted)
+
+# path to the .csv file in the ./transformed folder
+transf_path = Path("transformed/archive.csv")
+
+# read csv as dataframe
+# low_memory=False is needed because DtypeWarning: Columns (0) have mixed types"
+archives_df_v1 = pd.read_csv(transf_path, sep="\t", header=None, names=['id','marc_field','value'], compression='gzip', low_memory=False)
+
+# drop the first row because it contains the old column names
+archives_df_v2 = archives_df_v1.iloc[1:]
+
+# # to check counts before filling in empty values
+# print(archives_df_v2.info(verbose = True, show_counts = True))
+# print(archives_df_v2.head(100))
+
+# make a copy
+archives_df_v3 = archives_df_v2.reset_index(drop=True).copy()
+
+# convert the transformed data into a csv where each column is a marc field/subfield
+archives_df_v4 = archives_df_v3.pivot_table(index="id", columns="marc_field", values="value", aggfunc=lambda x: 'Ω'.join(x.dropna()))
+
+# # to check counts before filling in empty values
+# print(archives_df_v4.info(verbose = True, show_counts = True))
+
+archives_df_v5 = archives_df_v4.fillna("null")
+
+# # inspect the dataframe before storing it
+# print(archives_df_v5.info(verbose = True, show_counts = True))
+# print(archives_df_v5.describe())
+# print(archives_df_v5.head(10))
+
+# make a copy
+archives_df = archives_df_v5.reset_index(drop=True).copy()
+
+archives_df.to_csv('converted/archives_csv.csv', sep = '\t', index = False, compression = 'gzip')
+
+# print(archives_df_v5.head(100))
